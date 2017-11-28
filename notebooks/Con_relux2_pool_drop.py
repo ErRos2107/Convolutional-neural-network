962c19178576dba40b42d2e172c52a885b6d74ff
ShootingSpace <erdongerzong@qq.com>
2017-11-28 19:00:02 +0000

    CNN drop

--- notebooks/Con_relux2_pool_drop.py
+++ notebooks/Con_relux2_pool_drop.py
@@ -1,19 +1,21 @@
-'''mlp cw2 part A'''
+
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+import numpy as np
 from mlp.layers import AffineLayer, SoftmaxLayer, SigmoidLayer, ReluLayer, LeakyReluLayer, ELULayer, SELULayer
-from mlp.layers import DropoutLayer, RadialBasisFunctionLayer,  BatchNormalizationLayer
+from mlp.layers import ConvolutionalLayer, MaxPoolingLayer, ReshapeLayer, BatchNormalizationLayer, DropoutLayer
 from mlp.errors import CrossEntropySoftmaxError
 from mlp.models import MultipleLayerModel
 from mlp.initialisers import ConstantInit, GlorotUniformInit
-from mlp.learning_rules import GradientDescentLearningRule, RMSPropLearningRule, AdamLearningRule
+from mlp.learning_rules import GradientDescentLearningRule
+from mlp.learning_rules import RMSPropLearningRule, AdamLearningRule
 from mlp.optimisers import Optimiser
-import matplotlib.pyplot as plt
-import numpy as np
-import logging
-from mlp.data_providers import MNISTDataProvider, EMNISTDataProvider
-from collections import defaultdict
 from mlp.penalty import L1Penalty, L2Penalty
-print('         Stride     !!!!')
-plt.style.use('ggplot')
+from collections import defaultdict
+import logging
+import pickle
+from mlp.data_providers import MNISTDataProvider, EMNISTDataProvider
 
 def train_model_and_plot_stats(
         model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval, notebook=False):
@@ -48,29 +50,17 @@ def train_model_and_plot_stats(
     ax_2.legend(loc=0)
     ax_2.set_xlabel('Epoch number')
 
-    return stats, keys, run_time, fig_1, ax_1, fig_2, ax_2
-
-################################################################################
-# Seed a random number generator
-seed = 10102016
-rng = np.random.RandomState(seed)
-batch_size = 50
-# Set up a logger object to print info about the training run to stdout
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.handlers = [logging.StreamHandler()]
-
-# Create data provider objects for the EMNIST data set
-train_data = EMNISTDataProvider('train', batch_size=batch_size, rng=rng)
-valid_data = EMNISTDataProvider('valid', batch_size=batch_size, rng=rng)
+    return optimiser, stats, keys, run_time, fig_1, ax_1, fig_2, ax_2
 
 ################################################################################
 # save and present the data
-
-save_stats= defaultdict()
 def save_and_present(experiment, stats, parameter):
 
-    np.savetxt(experiment +'.csv', stats, delimiter=',')
+    # save the model to disk
+    filename = experiment +'.sav'
+    pickle.dump(model, open(filename, 'wb'))
+
+    np.savetxt(experiment+'_'+str(parameter)+'.csv', stats, delimiter=',')
 
     error_valid= stats[1:, keys['error(valid)']]
     error_train= stats[1:, keys['error(train)']]
@@ -92,81 +82,93 @@ def save_and_present(experiment, stats, parameter):
     print('Smallest error gap(after best acc epoch) = {} at Epoch={}'.
           format(min(overfitting[np.argmax(acc_valid):]),np.argmin(overfitting[np.argmax(acc_valid):])+np.argmax(acc_valid)+1))
 
+################################################################################
+# Seed a random number generator
+seed = 10102016
+rng = np.random.RandomState(seed)
+batch_size = 128
+# Set up a logger object to print info about the training run to stdout
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.handlers = [logging.StreamHandler()]
 
-######################################################################################################
-# Batch normalisation before Relu
-######################################################################################################
+# Create data provider objects for the EMNIST data set
+train_data = EMNISTDataProvider('train', batch_size=batch_size, rng=rng)
+valid_data = EMNISTDataProvider('valid', batch_size=batch_size, rng=rng)
+test_data = EMNISTDataProvider('test', batch_size=batch_size, rng=rng)
+####################################################################################################################################################
+# to ensure reproducibility of results
 rng.seed(seed)
 
 #setup hyperparameters
 learning_rate = 0.01
-num_epochs = 10
+num_epochs = 100
 stats_interval = 1
-input_dim, output_dim, hidden_dim = 784, 47, 256
+
+pad=0
+stride=1
+# kernel shape and feature maps
+num_output_channels1, num_output_channels2, kernel_dim_1, kernel_dim_2 = 5,10,3,3
+# Initial input, final output shape
+inputs_units, output_dim, hidden_dim = 784, 47, 256
+#####################################################################################################
+# Rehape to image shape for first convol
+num_input_channels, input_dim_1, input_dim_2 = 1, 28, 28
+# the ouput shape of the first convol layer is (batch_size, num_output_channels, Con_out_1, Con_out_1)
+Con_out_1 =  (input_dim_1 - kernel_dim_1+2*pad)//stride + 1
+# the ouput shape of the second convol layer is (batch_size, num_output_channels2, Con_out_2, Con_out_2)
+Con_out_2 = (Con_out_1 - kernel_dim_1+2*pad)//stride + 1
+# the ouput shape of the Maxpool
+Max_out = Con_out_2//2
+#####################################################################################################
+# then first fully connection input
+Fc_in = num_output_channels2* Max_out* Max_out
 
 weights_init = GlorotUniformInit(rng=rng)
 biases_init = ConstantInit(0.)
+#weights_penalty = L2Penalty(1e-3)
+##########################################################
+# Use p = 0.5 in hidden layers and 0.8 in the input layer.
+incl_prob_0 =0.8
+incl_prob = 0.5
+hidden_dim = int(hidden_dim/incl_prob)
+mom_coeff = 0.95
+##########################################################
 model = MultipleLayerModel([
-    AffineLayer(input_dim, hidden_dim, weights_init, biases_init),
-    BatchNormalizationLayer(hidden_dim),
+    ReshapeLayer((num_input_channels,input_dim_1,input_dim_2)),
+
+    ConvolutionalLayer(num_input_channels, num_output_channels1, input_dim_1, input_dim_2, kernel_dim_1, kernel_dim_2),
     ReluLayer(),
-    AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init),
-    BatchNormalizationLayer(hidden_dim),
+
+    ConvolutionalLayer(num_output_channels1, num_output_channels2, Con_out_1, Con_out_1, kernel_dim_1, kernel_dim_2),
     ReluLayer(),
+
+    MaxPoolingLayer(),
+
+	ReshapeLayer(),
+    DropoutLayer(rng, incl_prob_0),
+    AffineLayer(Fc_in, hidden_dim, weights_init, biases_init),
+    ReluLayer(),
+    DropoutLayer(rng, incl_prob),
     AffineLayer(hidden_dim, output_dim, weights_init, biases_init)
 ])
+##########################################################
 
 error = CrossEntropySoftmaxError()
-# Use a basic gradient descent learning rule
-learning_rule = AdamLearningRule(learning_rate=learning_rate,)
+# learning rule
+learning_rule = AdamLearningRule(learning_rate=learning_rate, mom_coeff=mom_coeff)
 
-experiment = 'BatchNorm_before_Relu_x2'
+experiment = 'Con_relux2_pool_drop'
 
-stats, keys, run_time, fig_1, ax_1, fig_2, ax_2 = train_model_and_plot_stats(
+#return stats, keys, run_time, fig_1, ax_1, fig_2, ax_2
+optimiser, stats, keys, run_time, fig_1, ax_1, fig_2, ax_2 = train_model_and_plot_stats(
     model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval, notebook=False)
 fig_1.savefig(experiment+ '_learning_rate_{}_error.pdf'.format(learning_rate))
-fig_2.savefig(experiment+'_learning_rate_{}_accuracy.pdf'.format(learning_rate))
+fig_2.savefig(experiment+ '_learning_rate_{}_accuracy.pdf'.format(learning_rate))
 
 save_and_present(experiment, stats, learning_rate)
 
-save_stats[experiment] = stats
-
-######################################################################################################
-# Batch normalisation after relu
-######################################################################################################
-rng.seed(seed)
-
-#setup hyperparameters
-learning_rate = 0.01
-num_epochs = 10
-stats_interval = 1
-input_dim, output_dim, hidden_dim = 784, 47, 256
-
-weights_init = GlorotUniformInit(rng=rng)
-biases_init = ConstantInit(0.)
-model = MultipleLayerModel([
-    AffineLayer(input_dim, hidden_dim, weights_init, biases_init),
-    ReluLayer(),
-    BatchNormalizationLayer(hidden_dim),
-    AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init),
-    ReluLayer(),
-    BatchNormalizationLayer(hidden_dim),
-    AffineLayer(hidden_dim, output_dim, weights_init, biases_init)
-])
-
-error = CrossEntropySoftmaxError()
-# Use a basic gradient descent learning rule
-learning_rule = AdamLearningRule(learning_rate=learning_rate,)
-
-experiment = 'BatchNorm_after_Relu_x2'
-
-stats, keys, run_time, fig_1, ax_1, fig_2, ax_2 = train_model_and_plot_stats(
-    model, error, learning_rule, train_data, valid_data, num_epochs, stats_interval, notebook=False)
-fig_1.savefig(experiment+ '_learning_rate_{}_error.pdf'.format(learning_rate))
-fig_2.savefig(experiment+'_learning_rate_{}_accuracy.pdf'.format(learning_rate))
-
-save_and_present(experiment, stats, learning_rate)
-
-save_stats[experiment] = stats
-
-
+save_results = defaultdict()
+save_results[experiment] = optimiser.eval_monitors(test_data, 'test')
+print('Test error:    ' + str(save_results[experiment]['errortest']))
+print('Test accuracy: ' + str(save_results[experiment]['acctest']))
